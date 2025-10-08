3621ad4ed90902a54519596c51d41fdb0ec7fdcc
josiluiz.nobre <josiluiz.nobre.043@ufrn.edu.br>
2025-10-07 22:25:28 -0300

    git push "git push" push git push --force origin <branch-name>

--- projeto/projetoU1_versao1.c
+++ projeto/projetoU1_versao1.c
@@ -21,6 +21,7 @@
 ******************************************************************************************/
 
 #include <stdio.h> // Incluir a biblioteca stdio.h
+#include <locale.h>
 
 // Declaração das funções
 
@@ -28,7 +29,7 @@ void digitarMatriz(int linhas, int colunas, int matriz[10][10]) {
   int i, j;
 
   // Escrever (preencher) a matriz com valores do utilizador
-  printf("Digite os elementos da matriz:\n");
+  //printf("Digite os elementos da matriz:\n");
   for (i = 0; i < linhas; i++) { // Loop pelas linhas
       for (j = 0; j < colunas; j++) { // Loop pelas colunas
           printf("Elemento [%d][%d]: ", i, j);
@@ -37,7 +38,7 @@ void digitarMatriz(int linhas, int colunas, int matriz[10][10]) {
   }
 
   // Ler (exibir) a matriz
-  printf("\nConteúdo da matriz:\n");
+  printf("\nConteudo da matriz:\n");
   for (i = 0; i < linhas; i++) { // Loop pelas linhas
       for (j = 0; j < colunas; j++) { // Loop pelas colunas
           printf("%d\t", matriz[i][j]); // Exibe o elemento
@@ -48,9 +49,9 @@ void digitarMatriz(int linhas, int colunas, int matriz[10][10]) {
 
 void somaMatrizes() {
     int linhaA, colunaA, linhaB, colunaB;
-    int matrizA[10][10];  // Fixed size array to avoid undefined behavior
-    int matrizB[10][10];  // Fixed size array to avoid undefined behavior
-    int matrizResultado[10][10];  // Matrix to store the result
+    int matrizA[10][10];  
+    int matrizB[10][10];  
+    int matrizResultado[10][10];  
   
     // Leitura das dimensões da primeira matriz
     printf("Digite os elementos da primeira matriz:\n");
@@ -67,7 +68,7 @@ void somaMatrizes() {
 
     // Verificar se as matrizes têm o mesmo tamanho
     if (linhaA != linhaB || colunaA != colunaB) {
-      printf("As matrizes não têm o mesmo tamanho. Não é possível realizar a soma.\n");
+      printf("As matrizes nao tem o mesmo tamanho. Nao e possivel realizar a soma.\n");
       return;
     } else {
       // Realizar a soma das matrizes
@@ -77,7 +78,7 @@ void somaMatrizes() {
         }
       }
       
-      printf("A soma das matrizes é:\n");
+      printf("A soma das matrizes e:\n");
       for (int i = 0; i < linhaA; i++) {
           for (int j = 0; j < colunaA; j++) {
               printf("%d\t", matrizResultado[i][j]);
@@ -87,51 +88,204 @@ void somaMatrizes() {
     }
 }  
 
-int main() {
+
+// Função subtração de matrizes
+void subtracaoMatrizes() {
+    int linhaA, colunaA, linhaB, colunaB;
+    int matrizA[10][10];  
+    int matrizB[10][10];  
+    int matrizResultado[10][10];  
+  
+    // Leitura das dimensões da primeira matriz
+    printf("Digite os elementos da primeira matriz:\n");
+    printf("Digite o tamanho da primeira matriz (linha coluna): ");
+    scanf(" %d %d", &linhaA, &colunaA);
+    digitarMatriz(linhaA, colunaA, matrizA);
+    printf("\n");
+  
+    // Leitura das dimensões da segunda matriz
+    printf("Digite os elementos da segunda matriz:\n");
+    printf("Digite o tamanho da segunda matriz (linha coluna): ");
+    scanf(" %d %d", &linhaB, &colunaB);
+    digitarMatriz(linhaB, colunaB, matrizB);
+
+    // Verificar se as matrizes têm o mesmo tamanho
+    if (linhaA != linhaB || colunaA != colunaB) {
+      printf("As matrizes nao tem o mesmo tamanho. Nao eh possivel realizar a subtracao.\n");
+      return;
+    } else {
+      // Realizar a subtração das matrizes
+      for (int i = 0; i < linhaA; i++) {
+        for (int j = 0; j < colunaA; j++) {
+            matrizResultado[i][j] = matrizA[i][j] - matrizB[i][j];
+        }
+      }
+      
+      printf("A subtração das matrizes é:\n");
+      for (int i = 0; i < linhaA; i++) {
+          for (int j = 0; j < colunaA; j++) {
+              printf("%d\t", matrizResultado[i][j]);
+          }
+          printf("\n");
+      }
+    }
+}  
+
+// Função produto de matrizes
+void produtoMatrizes() {
+  int linhaA, colunaA, linhaB, colunaB;
+  int matrizA[10][10];         // Fixed size array to avoid undefined behavior
+  int matrizB[10][10];         // Fixed size array to avoid undefined behavior
+  int matrizResultado[10][10]; // Matrix to store the result
+
+  // Leitura das dimensões da primeira matriz
+  printf("Digite os elementos da primeira matriz:\n");
+  printf("Digite o tamanho da primeira matriz (linha coluna): ");
+  scanf(" %d %d", &linhaA, &colunaA);
+  digitarMatriz(linhaA, colunaA, matrizA);
+  printf("\n");
+
+  // Leitura das dimensões da segunda matriz
+  printf("Digite os elementos da segunda matriz:\n");
+  printf("Digite o tamanho da segunda matriz (linha coluna): ");
+  scanf(" %d %d", &linhaB, &colunaB);
+  digitarMatriz(linhaB, colunaB, matrizB);
+
+  // Verificar se as matrizes podem ser multiplicadas
+  if (colunaA != linhaB) {
+    printf("O número de colunas da primeira matriz é diferente do número de "
+           "linhas da segunda. Não é possível realizar o produto.\n");
+    return;
+  } else {
+    // Inicializando a matriz de resultados com zeros para uso a seguir
+    for (int i = 0; i < linhaA; i++) {
+      for (int j = 0; j < colunaB; j++)
+        matrizResultado[i][j] = 0;
+    }
+
+    // Realizando o produto das matrizes
+    for (int i = 0; i < linhaA; i++) {
+      for (int j = 0; j < colunaB; j++)
+        for (int k = 0; k < colunaA; k++)
+          matrizResultado[i][j] += matrizA[i][k] * matrizB[k][j];
+    }
+
+    printf("A produto das matrizes é:\n");
+    for (int i = 0; i < linhaA; i++) {
+      for (int j = 0; j < colunaB; j++) {
+        printf("%d\t", matrizResultado[i][j]);
+      }
+      printf("\n");
+    }
+  }
+}
+
+// Função matriz transporte
+void transporteMatrizes() {
+  int linhaA, colunaA;
+  int matrizA[10][10];
+  int matrizResultado[10][10];
+
+  printf("Digite os elementos da matriz:\n");
+  printf("Digite o tamanho da matriz separados por espaco (linha coluna): ");
+  scanf(" %d %d", &linhaA, &colunaA);
+  digitarMatriz(linhaA, colunaA, matrizA);
+  printf("\n");
+
+  // Laço do transporte das matrizes
+  for (int i = 0; i < linhaA; i++) {
+    for (int j = 0; j < colunaA; j++)
+      matrizResultado[j][i] = matrizA[i][j];
+  }
+
+  printf("A matriz transporte sera:\n");
+  for (int i = 0; i < colunaA; i++) {
+    for (int j = 0; j < linhaA; j++) {
+      printf("%d\t", matrizResultado[i][j]);
+    }
+    printf("\n");
+  }
+}
+
+void determinanteMatrizes() {
+  int linhaA, colunaA;
+  int matrizA[10][10];
+  int matrizResultado[10][10];
+  int determinante = 0;
+
+  printf("Digite os elementos da matriz:\n");
+  printf("Digite o tamanho da matriz separados por espaco (linha coluna): ");
+  scanf(" %d %d", &linhaA, &colunaA);
+
+  if (linhaA != colunaA) {
+    printf("\n Nao existe determinante de matrizes nao quadradas.\n");
+    return;
+  }
+
+  digitarMatriz(linhaA, colunaA, matrizA);
+  printf("\n");
+
+  // Laço do determinante de matrizes
+  for (int i = 0; i < linhaA; i++) {
+    for (int j = 0; j < colunaA; j++) {
+      determinante += matrizA[i][j] * matrizA[j][i];
+    }  
+    determinante -= matrizA[i][i] * matrizA[i][i];
+  }
+  printf("O determinante da matriz eh: %d\n", determinante);
+}
+
+int main() {  
+  setlocale(LC_ALL, "Portuguese_Brazil");
   int operacao; // Variável para armazenar a operação a ser realizada;
 
+
   do {
-    printf("Qual a operação a ser realizada com matrizes?\n");
+    printf("Qual a operacao a ser realizada com matrizes?\n");
     printf("1 - Soma\n");
-    printf("2 - Subtração\n");
-    printf("3 - Multiplicação\n");
+    printf("2 - Subtracao\n");
+    printf("3 - Multiplicacao\n");
     printf("4 - Transporte\n");
     printf("5 - Determinante\n");
     printf("6 - Inversa\n");
     printf("7 - Ortogonal\n");
-    printf("8 - Equação Matricial\n");
+    printf("8 - Equacao Matricial\n");
     printf("9 - Sair\n");
     printf("\n");
     scanf("%d", &operacao);
 
     switch (operacao) {
       case 1: {
-        printf("Operação de soma selecionada.\n");
+        printf("Operacao de soma selecionada.\n");
         somaMatrizes();
         break;
       }  
       case 2: {
-        printf("Operação de subtração selecionada.\n");
+        printf("Operacao de subtracao selecionada.\n");
         // Chame a função para realizar a subtração de matrizes
+        subtracaoMatrizes();
         break;
       }
       case 3: {
-        printf("Operação de multiplicação selecionada.\n");
+        printf("Operacao de multiplicacao selecionada.\n");
         // Chame a função para realizar a multiplicação de matrizes
+        produtoMatrizes();
         break;
       }  
       case 4: {
-        printf("Operação de transporte selecionada.\n");
+        printf("Operacao de transporte selecionada.\n");
         // Chame a função para realizar o transporte de matrizes
+        transporteMatrizes();
         break;
       }
       case 5: {
-        printf("Operação de determinante selecionada.\n");
+        printf("Operacao de determinante selecionada.\n");
         // Chame a função para realizar o determinante de matrizes
+        determinanteMatrizes();
         break;
       }  
       case 6: {
-        printf("Operação de inversa selecionada.\n");
+        printf("Operacao de inversa selecionada.\n");
         // Chame a função para realizar a inversa de matrizes
         break;
       }  
@@ -151,6 +305,8 @@ int main() {
       }  
       default:
         printf("Operação inválida. Tente novamente.\n"); 
+
+
         break;
     } 
       
